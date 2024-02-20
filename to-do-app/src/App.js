7037285e1ba1c0c29be3f4509b22e53153acf223
Aman07122002 <amanvermavip6@gmail.com>
2024-02-21 02:56:37 +0530

    we have created a react app and design a basic structure of todo-app

---
+++ to-do-app/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="To-Do-App">
+      <h1>What is your plan today?</h1>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What is task title?" />
+          </div>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="What is task description?" />
+          </div>
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">Add</button>
+          </div>
+        </div>
+        
+        <div className="btn-area">
+          <button>Todo</button>
+          <button>Completed</button>
+        </div>
+
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <h1>Task 1</h1>
+            <p>Description</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
